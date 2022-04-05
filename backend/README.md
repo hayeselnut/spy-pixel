3f504967bfb9ab44b5cce8ec43d48e995e731666
Hayes Choy <hayeselnut2127@gmail.com>
2022-04-05 16:32:23 +1000

    locally logs each request to firestore database

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+# How to run locally
+
+```
+export FLASK_APP=main.py
+python3 -m flask run
+```
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from flask import Flask, send_file, make_response, request
 import base64
+import datetime
 import io
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('./spy-pixel-admin-sdk.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 
@@ -10,20 +20,30 @@ def hello_world():
 
 @app.route('/spy-pixel.gif')
 def spy_pixel():
-    print("===================================")
-    print("Campaign:", request.args.get("campaign"))
-    # print("User-Agent:", request.headers.get("User-Agent"))
-    # print("Host:", request.headers.get("Host"))
-    print(request.headers)
-    print(request.cookies)
-
-    print("===================================")
+    # print("===================================")
+    # print("Campaign:", request.args.get("campaign"))
+    # print(request.headers)
+    # print(request.cookies)
+    # print("===================================")
 
     if (request.args.get("campaign")):
         # Log to database
-        pass
+        log = {
+            u'timestamp': datetime.datetime.now(),
+            u'email': request.args.get("email"),
+            u'headers': dict(request.headers),
+            u'cookies': dict(request.cookies)
+        }
+
+        doc_ref = db.collection(u'campaigns').document(request.args.get("campaign"))
+        doc = doc_ref.get()
+
+        if not doc.exists:
+            doc_ref.set({u'logs': []})
+
+        doc_ref.update({u'logs': firestore.ArrayUnion([log])})
 
     gif_str = base64.b64decode('R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
     resp = make_response(send_file(io.BytesIO(gif_str), mimetype='image/gif'),200)
-    resp.set_cookie('userid', "this user doesnt mind cookies", max_age=1296000)
+    # resp.set_cookie('userid', "this user doesnt mind cookies", max_age=1296000)
     return resp
