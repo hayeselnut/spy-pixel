74551eaf5f4a4b26db82b8df82146ee88cc65a67
Hayes Choy <hayeselnut2127@gmail.com>
2022-04-07 20:25:39 +1000

    added instructions and added guards for XSS attack

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -16,24 +16,35 @@ const Dashboard = ({ campaigns }) => {
     setButtonicon("check");
   }
 
+  const regex = new RegExp("^[a-zA-Z0-9-_@+\.]+$");
+  const isAlphanumeric = (string) => {
+    return string === "" || regex.test(string);
+  }
+
   return (
     <Container style={{paddingTop: "4rem"}}>
       <Header as="h1">Dashboard</Header>
 
       <Header as="h2">Create a spy pixel!</Header>
 
+      <p>This button creates a spy pixel for you and copies it to your clipboard. Paste it into your emails and start sending!</p>
+      <p>Campaign IDs are compulsory as they are used to identify which email campaign the pixel is a part of. Emails are optional, and can be used to identify recipients. All input must be alphanumeric characters, ., -, _, @, + or .</p>
+      <p>NOTE: you can only paste into a rich text editor since HTML is copied directly to your clipboard.</p>
+
       <div style={{display: "flex", flexWrap: "wrap", rowGap: "1rem", columnGap: "1rem"}}>
         <div>
-          <Input list="campaigns" icon="flag" iconPosition="left" placeholder="Campaign ID" onChange={(e, d) => { setNewCampaignId(d.value); setButtonicon("copy"); }}/>
+          <Input error={!isAlphanumeric(newCampaignId)} list="campaigns" icon="flag" iconPosition="left" placeholder="Campaign ID" onChange={(e, d) => { setNewCampaignId(d.value); setButtonicon("copy"); }} />
         </div>
         <datalist id="campaigns">
           {campaigns.map(campaign => <option key={campaign.id} value={campaign.id}>{campaign.id}</option>)}
         </datalist>
+
         <div>
-          <Input icon="mail" iconPosition="left" placeholder="Email" onChange={(e, d) => { setNewEmail(d.value); setButtonicon("copy"); }}/>
+          <Input error={!isAlphanumeric(newEmail)} icon="mail" iconPosition="left" placeholder="Email" onChange={(e, d) => { setNewEmail(d.value); setButtonicon("copy"); }} />
         </div>
+
         <div>
-          <Button icon labelPosition="left" color={buttonIcon === "copy" ? "blue" : "green"} onClick={handleClick}>
+          <Button disabled={newCampaignId === "" || !isAlphanumeric(newCampaignId) || !isAlphanumeric(newEmail)} icon labelPosition="left" color={buttonIcon === "copy" ? "blue" : "green"} onClick={handleClick}>
             Copy to clipboard
             <Icon name={buttonIcon} />
           </Button>
