30caa43bdd4c1d515ce5c8631add216cf2ac03ca
Hayes Choy <hayeselnut2127@gmail.com>
2022-04-07 18:18:14 +1000

    campaign page loaded with headers and cookies info

--- frontend/src/pages/campaign.js
+++ frontend/src/pages/campaign.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Header } from 'semantic-ui-react';
+import { Accordion, Container, Header, Icon } from 'semantic-ui-react';
+import BackToHomeButton from '../components/back-to-home-button';
+import LogCookies from '../components/log-cookies';
+import LogHeaders from '../components/log-headers';
 
 const CampaignPage = ({ campaigns }) => {
-  let params = useParams();
-  console.log(campaigns);
+  const { campaignId } = useParams();
+  const [campaign] = campaigns.filter(campaign => campaign.id === campaignId);
+
+  if (!campaign) {
+    return (
+      <Container style={{paddingTop: '4rem'}}>
+        <BackToHomeButton />
+        <Header as='h1'>Campaigns {campaignId} not found</Header>
+      </Container>
+    )
+  }
+
+  const panels = campaign.logs.map((log, i) => ({
+    key: `panel-${i}`,
+    title: `${log.email || ""}${log.email ? " r" : "R"}ead at ${log.timestamp.toDate().toDateString()} ${log.timestamp.toDate().toLocaleTimeString()}`,
+    content: { content: <>
+      <Header as="h3" content="Headers"/>
+      <LogHeaders headers={log.headers} />
+
+      <Header as="h3" content="Cookies"/>
+      <LogCookies cookies={log.cookies}/>
+    </> }
+  }))
 
   return (
-    <Container style={{paddingTop: '2rem'}}>
-      <Header as='h1'>Campaigns Details for {params.campaignId}</Header>
+    <Container style={{paddingTop: '4rem'}}>
+      <BackToHomeButton />
+      <Header as='h1'><span style={{fontWeight: "normal"}}>Details for campaign </span>{campaignId}</Header>
+      <Header as='h2'>Logs</Header>
+      <Accordion styled fluid exclusive={false} panels={panels} />
     </Container>
   )
 }
