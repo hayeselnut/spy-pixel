51926c9dfb6c4e50ab6f40cdadbe24a820017222
Hayes Choy <hayeselnut2127@gmail.com>
2022-04-07 16:46:06 +1000

    response copy to clipboard button

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,48 +1,49 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Divider, Header, Input, Button } from 'semantic-ui-react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Divider, Header, Input, Button, Icon } from "semantic-ui-react";
 import copy from "copy-html-to-clipboard";
-import CampaignCard from '../components/campaign-card';
+import CampaignCard from "../components/campaign-card";
 
 const Dashboard = ({ campaigns }) => {
-  const [newCampaignId, setNewCampaignId] = useState('');
-  const [newEmail, setNewEmail] = useState('');
+  const [newCampaignId, setNewCampaignId] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [buttonIcon, setButtonicon] = useState("copy");
 
   const handleClick = () => {
-
-    console.log(`<img width="1" height="1" border="0" src="https://spy-pixel.ts.r.appspot.com/spy-pixel.gif?campaign=${newCampaignId}&email=${newEmail}" />`);
-    // const data = [new ClipboardItem({ ["text/html"]: `<img width="1" height="1" border="0" src="https://spy-pixel.ts.r.appspot.com/spy-pixel.gif?campaign=${newCampaignId}&email=${newEmail}" />` })]
     copy(`<img width="1" height="1" border="0" src="https://spy-pixel.ts.r.appspot.com/spy-pixel.gif?campaign=${newCampaignId}&email=${newEmail}" />`, {
       asHtml: true
     });
-    // navigator.clipboard.write(data);
+    setButtonicon("check");
   }
 
   return (
-    <Container style={{paddingTop: '4rem'}}>
-      <Header as='h1'>Dashboard</Header>
+    <Container style={{paddingTop: "4rem"}}>
+      <Header as="h1">Dashboard</Header>
 
-      <Header as='h2'>🆕 Create a spy pixel!</Header>
+      <Header as="h2">Create a spy pixel!</Header>
 
       <div style={{display: "flex", flexWrap: "wrap", rowGap: "1rem", columnGap: "1rem"}}>
         <div>
-          <Input list="campaigns" icon="flag" iconPosition="left" placeholder="Campaign ID" onChange={(e, d) => setNewCampaignId(d.value)}/>
+          <Input list="campaigns" icon="flag" iconPosition="left" placeholder="Campaign ID" onChange={(e, d) => { setNewCampaignId(d.value); setButtonicon("copy"); }}/>
         </div>
         <datalist id="campaigns">
           {campaigns.map(campaign => <option key={campaign.id} value={campaign.id}>{campaign.id}</option>)}
         </datalist>
         <div>
-          <Input icon="mail" iconPosition="left" placeholder="Email" onChange={(e, d) => setNewEmail(d.value)}/>
+          <Input icon="mail" iconPosition="left" placeholder="Email" onChange={(e, d) => { setNewEmail(d.value); setButtonicon("copy"); }}/>
         </div>
         <div>
-          <Button content="Copy to clipboard" onClick={handleClick}/>
+          <Button icon labelPosition="left" color={buttonIcon === "copy" ? "blue" : "green"} onClick={handleClick}>
+            Copy to clipboard
+            <Icon name={buttonIcon} />
+          </Button>
         </div>
       </div>
 
       <Divider />
 
-      <Header as='h2'>Active campaigns</Header>
-      <div style={{display: 'flex', flexWrap: 'wrap', rowGap: '2rem', columnGap: '2rem'}}>
+      <Header as="h2">Active campaigns</Header>
+      <div style={{display: "flex", flexWrap: "wrap", rowGap: "2rem", columnGap: "2rem"}}>
         {campaigns.map(campaign => (
           <Link to={`/campaign/${campaign.id}`} key={campaign.id}>
             <CampaignCard campaign={campaign} />
